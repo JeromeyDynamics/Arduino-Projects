51c206b91f622984039f844c4cec076878fa437a
AquaInfinite <aquainfinite64@gmail.com>
2024-06-14 17:30:07 -0400

    Update line_following_robot.ino

--- personal projects/line_following_robot/line_following_robot.ino
+++ personal projects/line_following_robot/line_following_robot.ino
@@ -1,9 +1,14 @@
+//constant variables
+#define IR_SENSOR_RIGHT 11
+#define IR_SENSOR_LEFT 12
+#define MOTOR_SPEED 180
+
 void setup() {
-  // put your setup code here, to run once:
+  
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
 
 }
